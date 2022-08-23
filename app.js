b0ca35d665c6c39ebc5e8229e8fa7ccc63583372
Otire82 <a.jeanne01@sncf.fr>
2022-08-23 18:20:36 +0200

    express3

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -19,6 +21,9 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", usersHandler.getUsers);
 app.get("/api/users/:id", usersHandler.getUsersById);
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", usersHandler.postUsers);
 
 app.listen(port, (err) => {
   if (err) {
